86b8baa6bde23dfea7ec9aabe3db48878c626605
stitou-nada <nadastito1087@gmail.com>
2022-01-14 13:42:38 +0000

    add

--- OOP (Ajouter-Afficher)/scripts.js
+++ OOP (Ajouter-Afficher)/scripts.js
@@ -11,7 +11,7 @@ document.getElementById("formSubmit").addEventListener("submit", function (event
     insertNewRow();
 
     resetForm();
-     
+
 })
 
 function resetForm() {
@@ -22,7 +22,7 @@ function resetForm() {
 
 function readOuvrage() {
     var ouvrage = new Ouvrage();
-    
+
     ouvrage.titre = document.getElementById("inputTitle").value;
     return ouvrage;
 }
@@ -32,16 +32,16 @@ function readOuvrage() {
 function insertNewRow() {
     var ouvrageList = gestionOuvrage.ouvrageList
     var tableBody = document.getElementById("ouvrageTable").getElementsByTagName('tbody')[0];
-   while(tableBody.rows.length > 0){
-       tableBody.deleteRow(0);
-   }
-   
-   for(var i = 0; i < ouvrageList.length; i++){
-    var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = ouvrageList[i].id;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = ouvrageList[i].titre;
-    cell3 = newRow.insertCell(2);
-   }
-  
+    while (tableBody.rows.length > 0) {
+        tableBody.deleteRow(0);
+    }
+
+    for (var i = 0; i < ouvrageList.length; i++) {
+        var newRow = tableBody.insertRow(tableBody.length);
+        cell1 = newRow.insertCell(0)
+        cell1.innerHTML = ouvrageList[i].id;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = ouvrageList[i].titre;
+    }
+
 }
