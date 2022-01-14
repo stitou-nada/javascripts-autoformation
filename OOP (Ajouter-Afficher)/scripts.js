f0b4fb909370e849e653511d914ab9e67ae41823
stitou-nada <nadastito1087@gmail.com>
2022-01-14 14:47:48 +0000

    add

--- OOP (Ajouter-Afficher)/scripts.js
+++ OOP (Ajouter-Afficher)/scripts.js
@@ -30,18 +30,18 @@ function readOuvrage() {
 
 
 function insertNewRow() {
-    var ouvrageList = gestionOuvrage.ouvrageList
+    var List = gestionOuvrage.ouvrageList
     var tableBody = document.getElementById("ouvrageTable").getElementsByTagName('tbody')[0];
     while (tableBody.rows.length > 0) {
         tableBody.deleteRow(0);
-    }
+    } 
 
-    for (var i = 0; i < ouvrageList.length; i++) {
+    for (var i = 0; i < List.length; i++) {
         var newRow = tableBody.insertRow(tableBody.length);
         cell1 = newRow.insertCell(0)
-        cell1.innerHTML = ouvrageList[i].id;
+        cell1.innerHTML = List[i].id;
         cell2 = newRow.insertCell(1);
-        cell2.innerHTML = ouvrageList[i].titre;
+        cell2.innerHTML = List[i].titre;
     }
 
 }
