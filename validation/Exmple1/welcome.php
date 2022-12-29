1220372820471c4eb04314861145018f278f9a57
stitou-nada <nadastito1087@gmail.com>
2022-12-29 13:57:48 +0100

    validation javascripts

---
+++ validation/Exmple1/welcome.php
@@ -0,0 +1,6 @@
+<h1>Welcome</h1>
+Name : <?php echo $_POST["name"]; ?><br>
+Email : <?php echo $_POST["email"]; ?><br>
+Mobile : <?php echo $_POST["mobile"]; ?><br>
+Address : <?php echo $_POST["address"]; ?><br>
+
